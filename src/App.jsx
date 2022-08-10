ef7d66a35755aa44a67a1eb49657783d35226eb2
shy <151686055@qq.com>
2022-08-11 03:57:19 +0800

    top layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 // import Layout from "./components/Layout";
 import './App.css';
 import Logo from "./components/Logo";
-import { ChakraProvider } from '@chakra-ui/react';
+import {Box, Spacer,Flex, ChakraProvider } from '@chakra-ui/react';
 
 
 const App = () => {
@@ -11,9 +11,12 @@ const App = () => {
     }, []);
     return (
         <ChakraProvider>
-            <div className="root-container">
-                <Logo className="logo-position" />
-            </div>
+            <Box bg="rgb(44,47,54)" w="100vw" h="100vh">
+                <Flex pl="20px" pt="4px" pr="20px" justify="space-between">
+                    <Logo />
+                    <Logo />
+                </Flex>
+            </Box>
         </ChakraProvider>
     )
 };
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -4,9 +4,13 @@ import {Icon, Flex, Box, Spacer,Center,Text} from '@chakra-ui/react';
 
 const Logo = ({className}) => {
     return (
-        <Flex className={className} color="blue.500" w="160px" h="40px">
-            <Center><Icon as={GiShamrock} boxSize="2em" /></Center>
-            <Center pl={1} fontSize="1.25em">AIOSWAP</Center>
+        <Flex color="blue.500" maxW="160px" h="38px">
+            <Center>
+                <Icon as={GiShamrock} boxSize="2em" />
+                <Box pl={1} fontSize="1.25em">AIOSWAP</Box>
+            </Center>
+            {/* <Center><Icon as={GiShamrock} boxSize="2em" /></Center>
+            <Center pl={1} fontSize="1.25em">AIOSWAP</Center> */}
         </Flex>
     )
 };
